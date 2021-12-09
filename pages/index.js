2f8d783f17189eb9c903097a7385e37668aa2df3
cedricklaucke <cedricklaucke@gmail.com>
2021-12-09 14:13:46 +0100

    added texts

--- pages/index.js
+++ pages/index.js
@@ -394,17 +394,20 @@ export default function Home() {
       {/* end side navbar */}
 
       {/* main content */}
-      <main className="flex flex-col w-full h-full space-y-6 mt-16 items-center">
+      <main className="flex flex-col w-full h-full space-y-6 mt-20 items-center">
         {/* 1st container */}
         {isDesktop ?
           /* desktop */
           <div className="flex flex-col md:flex-row w-11/12 md:h-96 border-2 rounded-2xl overflow-hidden dark:bg-black-100 dark:border-black-300">
-            <div className="flex flex-col w-full md:border-l-2 border-b-2 border-r-0 md:border-b-0 dark:border-black-300">
+            <div className="flex flex-col w-full md:border-r-2 border-b-2 border-r-0 md:border-b-0 dark:border-black-300">
               <div className="flex w-full border-b-2 h-16 items-center justify-center dark:border-black-300">
                 <span className="font-bold text-xl">Gamen</span>
               </div>
-              <div className="flex h-full items-center justify-center">
-                <span>main text</span>
+              <div className="flex flex-col h-full items-center justify-center">
+                <span>Een van mijn favoriete hobby's is gamen.</span>
+                <span>Het spel dat ik het meeste speel is World of Warcraft,</span>
+                <span>ik speel daarbij ook in een groep van 20 mensen</span>
+                <span>waarmee ik 3 keer in de week op vaste tijden game.</span>
               </div>
             </div>
             <div className="image relative flex">
@@ -426,8 +429,13 @@ export default function Home() {
               <div className="flex w-full border-b-2 h-16 items-center justify-center dark:border-black-300">
                 <span className="font-bold text-xl">Gamen</span>
               </div>
-              <div className="flex h-full items-center justify-center">
-                <span>main text</span>
+              <div className="flex flex-col h-full m-2 items-center justify-center">
+                <span>
+                  Een van mijn favoriete hobby's is gamen.
+                  Het spel dat ik het meeste speel is World of Warcraft,
+                  ik speel daarbij ook in een groep van 20 mensen
+                  waarmee ik 3 keer in de week op vaste tijden game.
+                </span>
               </div>
             </div>
             <div className="image relative flex">
@@ -465,8 +473,11 @@ export default function Home() {
               <div className="flex w-full border-b-2 h-16 items-center justify-center dark:border-black-300">
                 <span className="font-bold text-xl">Festivals</span>
               </div>
-              <div className="flex h-full items-center justify-center">
-                <span>main text</span>
+              <div className="flex flex-col h-full items-center justify-center">
+                <span>Ik ga graag naar festivals en feesten,</span>
+                <span>vooral hardstyle maar ook drum&bass en metal.</span>
+                <span>Mij favoriete festivals/feesten zijn:</span>
+                <span>Defqon.1, Supremacy en Let it Roll.</span>
               </div>
             </div>
           </div> :
@@ -476,8 +487,13 @@ export default function Home() {
               <div className="flex w-full border-b-2 h-16 items-center justify-center dark:border-black-300">
                 <span className="font-bold text-xl">Festivals</span>
               </div>
-              <div className="flex h-full items-center justify-center">
-                <span>main text</span>
+              <div className="flex flex-col h-full m-2 items-center justify-center">
+                <span>
+                  Ik ga graag naar festivals en feesten,
+                  vooral hardstyle maar ook drum&bass en metal.
+                  Mij favoriete festivals/feesten zijn:
+                  Defqon.1, Supremacy en Let it Roll.
+                </span>
               </div>
             </div>
             <div className="image relative flex">
@@ -499,12 +515,16 @@ export default function Home() {
         {isDesktop ?
           /* desktop */
           <div className="flex flex-col md:flex-row w-11/12 md:h-96 border-2 rounded-2xl overflow-hidden dark:bg-black-100 dark:border-black-300">
-            <div className="flex flex-col w-full md:border-l-2 border-b-2 border-r-0 md:border-b-0 dark:border-black-300">
+            <div className="flex flex-col w-full md:border-r-2 border-b-2 border-r-0 md:border-b-0 dark:border-black-300">
               <div className="flex w-full border-b-2 h-16 items-center justify-center dark:border-black-300">
                 <span className="font-bold text-xl">Boulderen</span>
               </div>
-              <div className="flex h-full items-center justify-center">
-                <span>main text</span>
+              <div className="flex flex-col h-full items-center justify-center">
+                <span>Een andere hobby is boulderen.</span>
+                <span>Dit is het klimmen van een korte route op een</span>
+                <span>vaak dicht bij de grond gelegen echte of kunstmatige rots</span>
+                <span>(ook wel een boulder genoemd) zonder het gebruik van</span>
+                <span>een touw of andere vorm van zekering.</span>
               </div>
             </div>
             <div className="image relative flex">
@@ -526,8 +546,14 @@ export default function Home() {
               <div className="flex w-full border-b-2 h-16 items-center justify-center dark:border-black-300">
                 <span className="font-bold text-xl">Boulderen</span>
               </div>
-              <div className="flex h-full items-center justify-center">
-                <span>main text</span>
+              <div className="flex flex-col h-full m-2 items-center justify-center">
+                <span>
+                  Een andere hobby is boulderen.
+                  Dit is het klimmen van een korte route op een
+                  vaak dicht bij de grond gelegen echte of kunstmatige rots
+                  (ook wel een boulder genoemd) zonder het gebruik van
+                  een touw of andere vorm van zekering.
+                </span>
               </div>
             </div>
             <div className="image relative flex">
