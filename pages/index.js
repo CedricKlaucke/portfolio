229c5e76dde979e2135f8cac63a0b8a129d56b7e
cedricklaucke <cedricklaucke@gmail.com>
2022-03-28 13:24:21 +0200

    fix vercel build

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import Head from "next/head";
-import Image from "next/image";
 import { motion } from "framer-motion";
-import useMediaQuery from "./hooks/useMediaQuery";
-import { Container } from "../components/index/container";
+import { Container } from "../components/index/Container";
 
 export default function Home() {
   return (
